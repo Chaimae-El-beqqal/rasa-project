d3058609d4812cfdb8ef858e0a4d45e790c32c60
Chaimae-El-beqqal <102841184+Chaimae-El-beqqal@users.noreply.github.com>
2023-09-06 15:43:11 +0100

    adding more features
    - updating training data
    - creating retrieval intents
    - rich responses :  buttons, images

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import yaml
 from flask import Flask, render_template, request, jsonify, Response
 import csv
 import os
@@ -20,15 +21,8 @@ def index():
 
 # Modify these paths accordingly
 data_folder = ""
+
 output_yaml_path = os.path.join(data_folder, "output.yml")
-
-
-# Append section content to output YAML
-def append_section_to_yaml(section_content):
-    with open(output_yaml_path, 'a', encoding='utf-8') as output_file:
-        output_file.write(section_content)
-        output_file.write("\n")  # Add a newline after each section
-
 
 config = '''assistant_id: default_config_bot
 language: "fr"  
@@ -47,6 +41,9 @@ pipeline:
   - name: EntitySynonymMapper
   - name: ResponseSelector
     epochs: 100
+  - name: ResponseSelector
+    epochs: 100
+
 
 policies:
 - name: MemoizationPolicy
@@ -62,16 +59,26 @@ policies:
 def upload():
     uploaded_files = request.files.getlist("files[]")
     filenames = ['nlu.csv', 'stories.csv', 'rules.csv', 'responses.csv']
-    expiration_time = request.form.get("expiration_time")
+    type = request.headers.get('Custom-Info')
+    # Create the 'uploads' directory if it doesn't exist
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
+
     for file, filename in zip(uploaded_files, filenames):
         file.save(os.path.join('uploads', filename))
-
-    generate_rasa_training_data(expiration_time)
+    if type == "new":
+        generate_rasa_training_data(60)
+    elif type =="add":
+        append_uploaded_data_to_existing_file(60, output_yaml_path)
     with open('output.yml', 'r', encoding='utf-8') as yaml_file:
         yaml_data = yaml_file.read()
-        print(yaml_data)
+        # print(yaml_data)
+    response =""
+    if type == "new":
+        response = {"message": "Files uploaded and Rasa training data generated successfully!"}
+    elif type =="add":
+        response = {"message": "Data added successfully!"}
 
-    response = {"message": "Files uploaded and Rasa training data generated successfully!"}
     return jsonify(response)
     # data_res = "Files uploaded and Rasa training data generated successfully!"
     # return render_template('index.html',data_res)
@@ -95,66 +102,87 @@ def generate_rasa_training_data(expiration_time):
                 if filename == 'nlu.csv':
                     for row in csv_reader:
                         intent_name = row['intent'].strip()
+                        # ============== this is for appending intents names in domain part  ==========================
                         if intent_name:
-                            nlu_intents.append(intent_name)
+                            # ------------- to check retrieval intents ----------
+                            if "/" in intent_name :
+                                name = intent_name.split("/")
+                                if not name[0] in nlu_intents:
+                                    nlu_intents.append(name[0])
+                            else :
+                                nlu_intents.append(intent_name)
+                        # =========         end of domain part  ========================================================
                         intent = row['intent']
-                        examples = row['examples'].split(',')
+                        examples = row['examples'].split(';')
                         nlu_data += f"- intent: {intent}\n"
                         nlu_data += "  examples: |\n"
 
                         for example in examples:
+                            # print("hello exp", example, "\n")
                             nlu_data += f"    - {example}\n"
+                    nlu_data+= "# NLU DATA PLACEHOLDER\n"
 
                 elif filename == 'stories.csv':
-                    current_story_actions = []
                     for row in csv_reader:
                         story = row['story']
-                        steps = row['steps'].split(',')
-
-                        current_story_actions = []  # Reset for each new story
-                        for i in range(0, len(steps)):
-                            element = steps[i].strip()
-                            if element.startswith("utter"):
-                                current_story_actions.append(element)
-                            else:
-                                intent = element
-                                current_story_actions.append(intent)
-                                # action = steps[i + 1].strip() if i + 1 < len(steps) else None
+                        steps = row['steps'].split(';')
 
                         stories_data += f"- story: {story}\n"
                         stories_data += "  steps:\n"
-                        for action in current_story_actions:
-                            if action.startswith("utter"):
-                                stories_data += f"  - action: {action}\n"
+                        for elt in steps:
+                            if elt.startswith("utter"):
+                                stories_data += f"  - action: {elt}\n"
                             else:
-                                stories_data += f"  - intent: {action}\n"
+                                stories_data += f"  - intent: {elt}\n"
+                    stories_data+= "# STORIES DATA PLACEHOLDER\n"
 
                 elif filename == 'rules.csv':
                     for row in csv_reader:
                         rule = row['rule']
-                        steps = row['steps'].split(',')
+                        steps = row['steps'].split(';')
                         rules_data += f"- rule: {rule}\n"
                         rules_data += "  steps:\n"
                         for i in range(0, len(steps)):
                             element = steps[i].strip()
                             if element.startswith("utter"):
                                 rules_data += f"  - action: {element}\n"
-
                             else:
                                 rules_data += f"  - intent: {element}\n"
-
+                    rules_data+="# RULES DATA PLACEHOLDER\n"
 
                 elif filename == 'responses.csv':
                     domain_content += "intents:\n"
                     for intent in nlu_intents:
                         domain_content += f"- {intent}\n"
+                    domain_content += "# INTENTS DATA PLACEHOLDER\n"
                     domain_content += "\nresponses:\n"
                     for row in csv_reader:
-                        utter_name = row['utter_name']
-                        response_text = row['text']
-                        domain_content += f"  {utter_name}:\n"
-                        domain_content += f"  - text: \"{response_text}\"\n"
+                        # =========== handle response name =================
 
+                        utter_name = row['utter_name']
+                        domain_content += f"  {utter_name}:\n"
+
+                        # =========== handle response texts =================
+                        text = row['text'].split(';')
+                        for i in range(0, len(text)):
+                            element = text[i].strip()
+                            domain_content += f"  - text: \"{element}\"\n"
+
+                        # =========== handle response buttons =================
+                        if row['buttons']:
+                            domain_content += f"    buttons: \n"
+                            btns = row['buttons'].split(';')
+                            # print(btns)
+                            for i in range(0, len(btns), 2):
+                                # print(btns[i], i)
+                                domain_content += f"    - title: \"{btns[i]}\"\n"
+                                domain_content += f"      payload: \"{btns[i + 1]}\"\n"
+
+                        # =========== handle response imgs =================
+                        if row['images']:
+                            img = row['images']
+                            domain_content += f"    image: \"{img}\"\n"
+                    domain_content+= "# DOMAIN DATA PLACEHOLDER\n"
     domain_content += "\nsession_config:\n"
     domain_content += f"  session_expiration_time: {expiration_time}\n"
     domain_content += "  carry_over_slots_to_new_session: true\n"
@@ -168,7 +196,113 @@ def generate_rasa_training_data(expiration_time):
     append_section_to_yaml(stories_data)
     append_section_to_yaml(rules_data)
 
+# ---------------------------------------- update the data files -----------------------------------------
 
+def append_uploaded_data_to_existing_file(expiration_time, output_yaml_path):
+    uploaded_files = os.listdir('uploads')
+    nlu_data = ""
+    stories_data = ""
+    rules_data = ""
+    nlu_intents = []
+    intents =""
+    domain_content = ""
+
+    for filename in uploaded_files:
+        csv_filename = os.path.join('uploads', filename)
+        if os.path.exists(csv_filename):
+            with open(csv_filename, 'r', encoding='utf-8-sig') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                if filename == 'nlu.csv':
+                    for row in csv_reader:
+                        intent_name = row['intent'].strip()
+                        if intent_name:
+                            if "/" in intent_name:
+                                name = intent_name.split("/")
+                                if not name[0] in nlu_intents:
+                                    nlu_intents.append(name[0])
+                                    print(name[0])
+                            else:
+                                nlu_intents.append(intent_name)
+
+                        intent = row['intent']
+                        examples = row['examples'].split(';')
+                        nlu_data += f"- intent: {intent}\n"
+                        nlu_data += "  examples: |\n"
+
+                        for example in examples:
+                            nlu_data += f"    - {example}\n"
+                    nlu_data+= "# NLU DATA PLACEHOLDER\n"
+
+                elif filename == 'stories.csv':
+                    for row in csv_reader:
+                        story = row['story']
+                        steps = row['steps'].split(';')
+
+                        stories_data += f"- story: {story}\n"
+                        stories_data += "  steps:\n"
+                        for elt in steps:
+                            if elt.startswith("utter"):
+                                stories_data += f"  - action: {elt}\n"
+                            else:
+                                stories_data += f"  - intent: {elt}\n"
+                    stories_data+= "# STORIES DATA PLACEHOLDER\n"
+
+                elif filename == 'rules.csv':
+                    for row in csv_reader:
+                        rule = row['rule']
+                        steps = row['steps'].split(';')
+                        rules_data += f"- rule: {rule}\n"
+                        rules_data += "  steps:\n"
+                        for i in range(0, len(steps)):
+                            element = steps[i].strip()
+                            if element.startswith("utter"):
+                                rules_data += f"  - action: {element}\n"
+                            else:
+                                rules_data += f"  - intent: {element}\n"
+                    rules_data+= "# RULES DATA PLACEHOLDER\n"
+
+                elif filename == 'responses.csv':
+
+                    for row in csv_reader:
+                        utter_name = row['utter_name']
+                        domain_content += f"  {utter_name}:\n"
+
+                        text = row['text'].split(';')
+                        for i in range(0, len(text)):
+                            element = text[i].strip()
+                            domain_content += f"  - text: \"{element}\"\n"
+
+                        if row['buttons']:
+                            domain_content += f"    buttons: \n"
+                            btns = row['buttons'].split(';')
+                            for i in range(0, len(btns), 2):
+                                domain_content += f"    - title: \"{btns[i]}\"\n"
+                                domain_content += f"      payload: \"{btns[i + 1]}\"\n"
+
+                        if row['images']:
+                            img = row['images']
+                            domain_content += f"    image: \"{img}\"\n"
+                    domain_content += "# DOMAIN DATA PLACEHOLDER\n"    
+
+    # Read the existing YAML file
+    with open(output_yaml_path, 'r', encoding='utf-8') as existing_yaml_file:
+        existing_yaml_data = existing_yaml_file.read()
+    # ============== TO ADD INTENTS NAME
+    if "intents:" in existing_yaml_data:
+        for intent in nlu_intents:
+            if f"- {intent}" not in existing_yaml_data:
+                intents += f"- {intent}\n"
+        intents += "# INTENTS DATA PLACEHOLDER\n"
+    # Replace the placeholders in the existing file with the new data
+    updated_yaml_data = existing_yaml_data.replace("# NLU DATA PLACEHOLDER", nlu_data)
+    updated_yaml_data = updated_yaml_data.replace("# STORIES DATA PLACEHOLDER", stories_data)
+    updated_yaml_data = updated_yaml_data.replace("# RULES DATA PLACEHOLDER", rules_data)
+    updated_yaml_data = updated_yaml_data.replace("# DOMAIN DATA PLACEHOLDER", domain_content)
+    updated_yaml_data = updated_yaml_data.replace("# INTENTS DATA PLACEHOLDER", intents)
+
+    # Write the updated data back to the YAML file
+    with open(output_yaml_path, 'w', encoding='utf-8') as updated_yaml_file:
+        updated_yaml_file.write(updated_yaml_data)
 @app.route('/train', methods=['POST'])
 def send_yaml():
     try:
@@ -188,7 +322,7 @@ def send_yaml():
         # Check response status and return result
         if response.status_code == 200:
             head = response.headers
-            print(head.get('filename'))
+            # print(head.get('filename'))
             # print()
             global model_path
             model_path = "C:/Users/Lenovo/Downloads/pythonProject/models/" + head.get(
@@ -198,7 +332,7 @@ def send_yaml():
         else:
             return jsonify({"error": "Request failed"}), response.status_code
     except requests.exceptions.RequestException as e:
-        return jsonify({"error": "An error occurred"}), response.status_code
+        return jsonify({"error": "An error occurred"})
 
 
 @app.route('/reload', methods=['POST'])
@@ -223,6 +357,23 @@ def send_reload_request_to_rasa():
             return jsonify({"error": "Request failed"}), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": "An error occurred"}), 500
+
+
+
+# Append section content to output YAML
+def append_section_to_yaml(section_content):
+    with open(output_yaml_path, 'a', encoding='utf-8') as output_file:
+        output_file.write(section_content)
+        output_file.write("\n")  # Add a newline after each section
+
+def get_section_from_yaml(yaml_file_path, section_name):
+    with open(yaml_file_path, 'r', encoding='utf-8') as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+
+    return yaml_data.get(section_name)
+
+def convert(data_dict):
+    return yaml.dump(data_dict, default_flow_style=False)
 
 
 if __name__ == '__main__':
